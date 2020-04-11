9bdd2b9c6e463b823b79e6c33d76915541b1be2a
jmwakz99 <kemsjunior99@gmail.com>
2020-04-12 01:34:22 +0300

    index changes

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res, next) => {
   res.sendFile(path.join(__dirname, "./", "views", "index.html"));
 });
